3824db6e7cdafe08aa379cbe8333877f71487a33
PsaoFlamand <72673573+PsaoFlamand@users.noreply.github.com>
2021-09-16 16:07:08 -0700

    Add files via upload
    Primitive script to grab the transit route information from bc transit website.
    
    Improvement that can be made:
    - Use window search instead of regular expressions for the trip information gathering in order to enhance search robustness. (With current regular expression, anomalous characters in street name can cause bug)

---
+++ PyTransitScore.py
@@ -0,0 +1,67 @@
+import requests
+import re
+
+
+def main():
+    city_urls = get_city_urls(url)
+    route_info = get_route_info(city_urls)
+    
+    for route in route_map:
+        print(route_map[route])
+        input()
+
+def get_city_urls(url):
+    '''grab the source page from the root transit website in order to compile a list of all available cities for rating'''
+    print(url)
+    source_page = requests.get(url).content
+    
+    '''Extract the urls related to each city from the source page'''
+    
+    '''Uses a pattern search that will look for any occurences of community-url followed by anything but a space (\S+) followed by any number of spaces (\s+) followed by anything but spaces (\S+)'''
+    '''Proceeds to loop through the list of patterns found and parses them using splits'''
+    '''Sample output from the regular expression below: community-url" href="/100-mile-house/home">100'''
+    '''We then parse out the url we are interested in by splitting the string at href=" and taking only the right hand side ([-1] last element in the list) sample stage output: /100-mile-house/home">100'''
+    '''Now to isolate the url, we split by "> and take only the left hand side ([0] first element). Final output: /100-mile-house/home'''
+    '''Lastly, to get rid of any duplicates in the list, we turn it into a set, and then back into a list. This is because sets by definition only contain unique values'''
+
+    city_urls = list(set([x.split('href="')[-1].split('">')[0] for x in re.findall(r'href="\/\S+',str(source_page)) if 'home' in x]))
+
+    return city_urls
+
+        
+def get_route_info(city_urls):
+    route_map = {}
+    for city_url in city_urls:
+        if test_city not in city_url:
+            continue
+        
+        refined_city_routes_url = city_url.replace('home','schedules-and-maps')[1:]
+        city_url = '%s%s'%(url,refined_city_routes_url)
+        city_source_page = requests.get(city_url).content
+        
+        '''Similar method to above'''
+        route_urls = [x.split('href="')[-1].split('">')[0] for x in re.findall(r'data-route=\"\d+\"\s+\S+',str(city_source_page))]
+        
+        for route_url in route_urls:
+            route_url = '%s%s'%(url,route_url[1:])
+            route_source_page = requests.get(route_url).content
+            
+            route_number = re.findall(r'route=\d+',route_url)[0].split('route=')[-1]
+            trip_info = [x for x in re.findall(r"trip departs[-\#_:<>\/'\"\\a-zA-Z0-9\.\(\)\& ]+", str(route_source_page))]
+            
+            for trip in trip_info:
+                try:
+                    street = re.findall(r"'[-\#_:<>\/'\"\\a-zA-Z0-9\.\(\)\& ]+'",trip)[0].strip("'").strip('\\')
+                    time = re.findall(r'\d+:\d+\s[A-Z][A-Z]',trip)[0]
+                except IndexError:
+                    print('\n\n\nERROR: Regular Expression fails on: %s \n %s \n\n\n' % (route_url, trip))
+                    input()
+                    
+                try:    route_map[route_number].append('%s|%s'%(street, time))
+                except: route_map[route_number] = ['%s|%s'%(street, time)]
+        
+
+if __name__ == '__main__':
+    url = 'https://www.bctransit.com/'
+    test_city = 'kamloops'
+    main()
