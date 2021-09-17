15fe111d80c27c44c7a39c21e58ef2f61d9956ed
PsaoFlamand <72673573+PsaoFlamand@users.noreply.github.com>
2021-09-17 15:55:12 -0700

    Update PyTransitScore.py
    Added average frequency of bus stop

--- PyTransitScore.py
+++ PyTransitScore.py
@@ -7,17 +7,46 @@ import routingpy as rp
 def main():
     city_urls = get_city_urls(url)
     route_info = get_route_info(city_urls)
-
+    
+    print(f'Input Address {input_address}')
+    
     with open('results.txt','w') as result:    
         for route in route_info:
             for street in route_info[route]:
-                print('street',street)
-                route_duration, route_distance= get_route_distance_duration('1018 Calder Court, Kelowna, Canada', f'{street}, Kelowna, Canada')
-                print(f'route_duration:{route_duration} | route_distance: {route_distance}')
-                result.write(str('\n %s | %s | %s \n'%(route,street,route_info[route][street])))
-                print('\n %s | %s | %s \n'%(route,street,route_info[route][street]))
+                route_duration, route_distance= get_route_distance_duration(input_address, f'{street}, Kelowna, Canada')
+                average_route_frequency = get_route_frequency(route_info[route][street])
+                result.write(str(f'\n {route} | {street} | {average_route_frequency} \n'))
+                
+                print(f'\n{route} | Bus Stop: {street} | Average Frequency {average_route_frequency} Minutes \nTravel Time by Foot: {route_duration} minutes | Distance: {route_distance} km \n')
                 input()    
 
+def get_route_frequency(route_arrival_times):
+    frequency_list = []
+    for count, arrival_time in enumerate(route_arrival_times):
+        hours = int(arrival_time.split(':')[0])
+        minutes = int(arrival_time.split(':')[-1][:-3])
+
+        if arrival_time[-2:]=='PM' and arrival_time[:2]!='12':
+            hours+=12
+        elif arrival_time[-2:]=='AM' and arrival_time[:2]=='12':
+            hours+=12
+            
+        total_minutes = hours*60 + minutes
+        if count>0:
+            if total_minutes-prev_total_minutes > 0:
+                frequency_list.append(total_minutes-prev_total_minutes)
+                
+        prev_arrival_time = arrival_time
+        prev_hours = hours
+        prev_minutes = minutes
+        prev_total_minutes = total_minutes
+        
+    average_frequency = 0
+    for frequency in frequency_list:
+        average_frequency += frequency
+    average_frequency /= len(frequency_list)
+
+    return round(average_frequency,3)
 
 def get_city_urls(url):
     '''grab the source page from the root transit website in order to compile a list of all available cities for rating'''
@@ -104,8 +133,7 @@ def get_location_by_address(address):
 
 
 def get_route_distance_duration(origin_address, destination_address):
-    print('origin_address',origin_address)
-    print('destination_address',destination_address)
+
     origin_location = get_location_by_address(origin_address)
     origin  = [float(origin_location["lon"]), float(origin_location["lat"])]
 
@@ -115,14 +143,14 @@ def get_route_distance_duration(origin_address, destination_address):
     coordinates = [origin, destination]
     api = rp.ORS(api_key="5b3ce3597851110001cf6248993396cdbce943a293d5fcf34c10bcee")
 
-    route = api.directions(locations=coordinates, profile='driving-car')
+    route = api.directions(locations=coordinates, profile='foot-walking')
 
-    print(f'Duration: {route.duration/60} minutes \nDistance: {route.distance/1000} km')
-    return route.duration/60, route.distance/1000
+    return round(route.duration/60,3), round(route.distance/1000,3)
 
 
 if __name__ == '__main__':
     app = Nominatim(user_agent="tutorial")
     url = 'https://www.bctransit.com/'
     test_city = 'kelowna'
+    input_address = '1018 Calder Court, Kelowna, Canada'
     main()
