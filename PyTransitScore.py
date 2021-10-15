e0251a1345868b1c1701e8d452b32928802c1dc0
PsaoFlamand <72673573+PsaoFlamand@users.noreply.github.com>
2021-10-14 23:35:05 -0700

    Update PyTransitScore.py
    Changed to calculating distance "As the crow flies".
    Added source data for gps coordinates of addresses and bus stops.

--- PyTransitScore.py
+++ PyTransitScore.py
@@ -1,52 +1,134 @@
-import requests
+import geopy.distance
 import re
-from geopy.geocoders import Nominatim
 import time
-import routingpy as rp
+import os
+import requests
+
 
 def main():
+    '''Time the whole process'''
+    t0 = time.time()
+    
+    '''Scrape BC transit website for data to rank routes by'''
     city_urls = get_city_urls(url)
     route_info = get_route_info(city_urls)
-    
-    print(f'Input Address {input_address}')
-    
-    with open('results.txt','w') as result:    
-        for route in route_info:
-            for street in route_info[route]:
-                route_duration, route_distance= get_route_distance_duration(input_address, f'{street}, Kelowna, Canada')
-                average_route_frequency = get_route_frequency(route_info[route][street])
-                result.write(str(f'\n {route} | {street} | {average_route_frequency} \n'))
-                
-                print(f'\n{route} | Bus Stop: {street} | Average Frequency {average_route_frequency} Minutes \nTravel Time by Foot: {route_duration} minutes | Distance: {route_distance} km \n')
-                input()    
 
-def get_route_frequency(route_arrival_times):
-    frequency_list = []
-    for count, arrival_time in enumerate(route_arrival_times):
-        hours = int(arrival_time.split(':')[0])
-        minutes = int(arrival_time.split(':')[-1][:-3])
+    '''gather address, bus stop coords'''
+    address_map, bus_stop_map, route_map = parse_source_data(source_folder)
 
-        if arrival_time[-2:]=='PM' and arrival_time[:2]!='12':
-            hours+=12
-        elif arrival_time[-2:]=='AM' and arrival_time[:2]=='12':
-            hours+=12
+    '''Associate routes with stops to allow us to weight stops according to active frequency'''
+    route_bound_stops = associate_routes_with_stops(bus_stop_map,route_map)
+    
+    '''get route rankings'''
+    route_rankings = rank_routes(route_info)
+    normalized_ranking = normalize_ranking(route_rankings)
+    print(normalized_ranking)
+
+    ''', and dict with number of close stops'''
+    address_rankings = rank_addresses(address_map,bus_stop_map)
+
+    print(time.time() - t0)
+
+###WIP###
+def associate_routes_with_stops(bus_stop_map,route_map):
+    route_bound_stops=0
+    return route_bound_stops
+
+
+def rank_addresses(address_map, bus_stop_map):
+    address_rankings = dict()
+
+    '''Sort by number of bus stops within 400m'''        
+    for address in address_map:
+        stops_within_range = 0
+        for bus_stop in bus_stop_map:
+            stop_distance = geopy.distance.distance(address_map[address], bus_stop_map[bus_stop])
             
-        total_minutes = hours*60 + minutes
-        if count>0:
-            if total_minutes-prev_total_minutes > 0:
-                frequency_list.append(total_minutes-prev_total_minutes)
+            if stop_distance < 0.4:
+                stops_within_range += 1
+            elif stop_distance < 0.8:
+                stops_within_range += 0.5
                 
-        prev_arrival_time = arrival_time
-        prev_hours = hours
-        prev_minutes = minutes
-        prev_total_minutes = total_minutes
-        
-    average_frequency = 0
-    for frequency in frequency_list:
-        average_frequency += frequency
-    average_frequency /= len(frequency_list)
+        address_rankings[address] = stops_within_range
+    
+    to_be_ranked = dict(sorted(address_rankings.items(), key=lambda x:x[1]))
+    
+    return address_rankings
 
-    return round(average_frequency,3)
+###
+
+def parse_source_data(source_folder):
+    bus_stop_map = dict()
+    address_map = dict()
+    route_map = dict()
+    
+    with open(os.path.join(source_folder,bus_stop_file)) as bus_stop_points,\
+         open(os.path.join(source_folder,address_file)) as address_points,\
+         open(os.path.join(source_folder,route_file)) as route_points:
+             
+        address_lines = address_points.read()
+        bus_stop_lines = bus_stop_points.readlines()
+        route_lines = route_points.read()
+        
+        '''Parse bus stop coords'''
+        for bus_line in bus_stop_lines:
+            if not bus_line.split(',')[3].isalpha():
+                bus_lat = bus_line.split(',')[3]
+                bus_lon = bus_line.split(',')[4]
+                bus_stop_map[bus_line.split(',')[1]] = (bus_lat, bus_lon)
+                
+        '''Parse address street and gps coordinate'''
+        raw_address_info = re.findall(r'FULL_ADD(.*?) ] } }',address_lines)[0:100]
+
+        for address_line in raw_address_info:
+            street = address_line.split(':')[1].split(',')[0]
+            raw_coords = address_line.split(':')[-1].replace('[','').strip().split(',')
+
+            address_lat = float(raw_coords[1])
+            address_lon = float(raw_coords[0])
+            
+            address_map[street] = (address_lat,address_lon)
+
+        route_coords = re.findall(r'<coordinates>(.*?)</coordinates>',route_lines)
+        route_names = re.findall(r'Route: \d+',route_lines)
+        for route_coord, route_name in zip(route_coords,route_names):
+            try:    route_map[route_name].append(route_coord.strip('<coordinates>').split(' '))
+            except: route_map[route_name] = [route_coord.strip('<coordinates>').split(' ')]
+            
+            
+    return address_map, bus_stop_map,route_map
+
+
+
+def normalize_ranking(dict_in):
+    normalized_ranking = dict()
+
+    xmin = min(dict_in.values())
+    xmax = max(dict_in.values())
+        
+    for xi in dict_in:
+        normalized_ranking[xi] = (dict_in[xi] - xmin) / (xmax - xmin)
+
+    normalized_ranking = dict(sorted(normalized_ranking.items(), key=lambda x: x[1]))
+    return normalized_ranking
+
+    
+def rank_routes(route_info):
+    route_ranking = dict()
+   
+    for route in route_info:
+        total_average = 0
+
+        for street in route_info[route]:
+            average_route_frequency = get_route_frequency(route_info[route][street])
+            if average_route_frequency == 0:
+                average_route_frequency=999
+            total_average += average_route_frequency
+
+        route_ranking[route] = total_average / len(route_info[route])
+
+    return route_ranking
+
 
 def get_city_urls(url):
     '''grab the source page from the root transit website in order to compile a list of all available cities for rating'''
@@ -89,24 +171,17 @@ def get_route_info(city_urls):
             '''Same method as before'''
             route_url = '%s%s'%(url,route_url[1:])
             route_source_page = requests.get(route_url).content
-            with open('route_source_page.txt','w') as bug:
-                bug.write(str(route_source_page))
+            
             '''finds the pattern route=\d+ (\d+ = more than one digit), and isolates the digit by splitting at route= and taking the second half'''
             route_number = re.findall(r'route=\d+',route_url)[0]
             
-            '''finds occurences of trip departs along with the chars contained in between the [...]. Will implement window search to make more robust'''
-            trip_info = [x for x in re.findall(r"trip departs[-\#_:<>\/'\"\\a-zA-Z0-9\.\(\)\& ]+", str(route_source_page))]
+            '''finds occurences of trip departs along with the chars contained in between the [...]. Will implement window search to make more robust ###Fixed with a better regex'''
+            trip_info = [x for x in re.findall(r"trip departs(.*?)<span class", str(route_source_page))]
             
             for trip in trip_info:
-                try:
-                    '''Same method as above'''
-                    street = re.findall(r"'[-\#_:<>\/'\"\\a-zA-Z0-9\.\(\)\& ]+'",trip)[0].strip("'").strip('\\')
-                    time = re.findall(r'\d+:\d+\s[A-Z][A-Z]',trip)[0]
-                except IndexError:
-                    '''In the case that the above string returned by the regex is not found, it is likely due to the regex pattern missing some anomalous chars. '''
-                    '''Window search would fix this issue'''
-                    print('\n\n\nERROR: Regular Expression fails on: %s \n %s \n\n\n' % (route_url, trip))
-                    input()
+                '''Same method as above'''
+                street = re.findall(r"'(.*?)'",trip)[0].strip("'").strip('\\')
+                time = re.findall(r'\d+:\d+\s[A-Z][A-Z]',trip)[0]
                     
                 '''Builds a list in a dictionary so that we can store the street and time of arrival for each bus route'''
                 '''we use the try except block in because we can't know wether or not the list has been preassigned or not, so we assume it exists. '''
@@ -122,35 +197,47 @@ def get_route_info(city_urls):
     return route_map
 
 
-def get_location_by_address(address):
-    """This function returns a location as raw from an address
-    will repeat until success"""
-    time.sleep(0.1)#Not sure if needed
-    try:
-        return app.geocode(address).raw
-    except:
-        return get_location_by_address(address)
+def get_route_frequency(route_arrival_times):
+    frequency_list = []
+    for count, arrival_time in enumerate(route_arrival_times):
+        hours = int(arrival_time.split(':')[0])
+        minutes = int(arrival_time.split(':')[-1][:-3])
 
-
-def get_route_distance_duration(origin_address, destination_address):
-
-    origin_location = get_location_by_address(origin_address)
-    origin  = [float(origin_location["lon"]), float(origin_location["lat"])]
-
-    destination_location = get_location_by_address(destination_address)
-    destination  = [float(destination_location["lon"]), float(destination_location["lat"])]
-
-    coordinates = [origin, destination]
-    api = rp.ORS(api_key="5b3ce3597851110001cf6248993396cdbce943a293d5fcf34c10bcee")
-
-    route = api.directions(locations=coordinates, profile='foot-walking')
-
-    return round(route.duration/60,3), round(route.distance/1000,3)
+        if arrival_time[-2:]=='PM' and arrival_time[:2]!='12':
+            hours+=12
+        elif arrival_time[-2:]=='AM' and arrival_time[:2]=='12':
+            hours+=12
+            
+        total_minutes = hours*60 + minutes
+        if count>0:
+            if total_minutes-prev_total_minutes > 0:
+                frequency_list.append(total_minutes-prev_total_minutes)
+                
+        prev_arrival_time = arrival_time
+        prev_hours = hours
+        prev_minutes = minutes
+        prev_total_minutes = total_minutes
+        
+    average_frequency = 0
+    
+    if frequency_list:
+        for frequency in frequency_list:
+            average_frequency += frequency
+        average_frequency /= len(frequency_list)
+        
+    return round(average_frequency,3)
 
 
 if __name__ == '__main__':
-    app = Nominatim(user_agent="tutorial")
+    '''data setup'''
+    source_folder = 'source_data'
+    bus_stop_file = 'kel_busstops.csv'
+    route_file = 'kel_routes.kml'
+    address_file = 'Address_Points.geojson'
+    
+    '''Scraping setup'''
     url = 'https://www.bctransit.com/'
     test_city = 'kelowna'
-    input_address = '1018 Calder Court, Kelowna, Canada'
+    
     main()
+
