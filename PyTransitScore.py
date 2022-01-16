1d4eb9d899313db1bcc8f5803ab5f3217c4f7c06
PsaoFlamand <72673573+PsaoFlamand@users.noreply.github.com>
2022-01-16 00:11:33 -0800

    Add ability to get distance to a given amenity

--- PyTransitScore.py
+++ PyTransitScore.py
@@ -1,47 +1,50 @@
+import re,os,time,math,requests
 import geopy.distance
-import re
-import time
-import os
-import requests
 from pyroutelib3 import Router
-import math
+from scipy import spatial
 
 
 def main():
     '''Time the whole process'''
     t0 = time.time()
-    
-    '''gather address, bus stop coords'''
-##    address_map, bus_stop_map, route_map = parse_source_data(source_folder)
 
-    
+    '''gather target, bus stop coords'''
+    address_map, bus_stop_map, route_map, grid_map = parse_source_data(source_folder)
+
     '''Scrape BC transit website for data to rank routes by'''
 ##    city_urls = get_city_urls(url)
 ##    route_info = get_route_info(city_urls)
 
-
     '''get route rankings'''    
 ##    route_rankings = rank_routes(route_info)
 ##    normalized_ranking = normalize_ranking(route_rankings)
-##    print(normalized_ranking)
-
-
+    
     '''Associate routes with stops to allow us to weight stops according to active frequency'''
 ##    route_bound_stops = associate_routes_with_stops(bus_stop_map,route_map)
-##    print(len(route_bound_stops))
 
-    
     '''Get a list of all the coordinates for the element in the coordinate list'''
-    for amenity in amenities:
-        amenty_results = get_amenity_coordinates(amenity)
-        print('%s | %s | %s\n' % (amenity, len(amenty_results), amenty_results))
-
-  
+    bus_stop_with_amenity_ranks = get_bus_stop_amenity_rank(bus_stop_map)
+    
     '''Dict with number of close stops'''
-##    address_rankings = rank_addresses(address_map,bus_stop_map)    
-##    with open('results.txt','w') as result:
-##        result.write(str(address_rankings))
+    #nearest_stop_map = get_closest_stop(address_map,ordered_bus_stop_with_amenity_ranks)
 
+    '''Grid Rankings'''
+    nearest_stop_map = get_closest_stop(grid_map, bus_stop_with_amenity_ranks)
+        
+    '''Hop around the nearest neighbors until a non zero amenity score is found'''
+    distances_from_origin_to_amenities = get_distances_from_origin_to_amenities(nearest_stop_map, bus_stop_with_amenity_ranks)
+
+    '''merge distances into a singular score to be mapped'''
+    for origin in distances_from_origin_to_amenities:
+        sum = 0
+        for distances in distances_from_origin_to_amenities[origin]:
+            for distance in distances:
+                sum += float(distance.km)
+                
+        #print('%s | %s' % (origin,sum))#distances_from_origin_to_amenities[origin]))
+
+
+    '''Print time taken'''
     print(time.time() - t0)
 
 
@@ -51,25 +54,125 @@ def associate_routes_with_stops(bus_stop_map,route_map):
     return route_bound_stops
 
 
-def get_amenity_coordinates(amenity):
-    amenity_coordinates = []
+def stop_hopper(stop_map,amenity_type,nearest_stop,index):
+
+    bus_list = [x for x in stop_map]
+    tree = spatial.KDTree(bus_list)
+    
+    def get_neighbor(kp):
+        nearest_neighbor = tree.query([nearest_stop], k = kp)
+
+        for neighbor in nearest_neighbor[1][0]:
+            if stop_map[bus_list[int(neighbor)]][amenity_type] != math.inf:
+                return neighbor
+            else:
+                amenity_not_found = 1
+                
+        if amenity_not_found:
+            return(get_neighbor(kp+1))
+            
+    return(get_neighbor(300))
+    
+    
+def get_distances_from_origin_to_amenities(nearest_stop_map,ordered_bus_stop_with_amenity_ranks):
+    distances_from_origin_to_amenities = dict()
+    for origin in nearest_stop_map:
+        for amenity_type in range(len(AMENITIES)):
+            nearest_stop = nearest_stop_map[origin][0]
+            distance_to_nearest_stop = nearest_stop_map[origin][1]
+
+            amenity_scores = ordered_bus_stop_with_amenity_ranks[nearest_stop]
+
+            if amenity_scores[amenity_type] == math.inf:
+                index = list(ordered_bus_stop_with_amenity_ranks).index(nearest_stop)
+                nearest_stop_with_amenity = stop_hopper(ordered_bus_stop_with_amenity_ranks,amenity_type,nearest_stop, index)
+                distance_between_nearest_stop_and_one_containing_amenity = geopy.distance.distance(nearest_stop, list(ordered_bus_stop_with_amenity_ranks)[nearest_stop_with_amenity])
+
+                try:
+                    distances_from_origin_to_amenities[origin].append([nearest_stop_map[origin][1],distance_between_nearest_stop_and_one_containing_amenity])
+                except Exception:
+                    distances_from_origin_to_amenities[origin] = [[nearest_stop_map[origin][1],distance_between_nearest_stop_and_one_containing_amenity]]
+            else:
+                try:
+                    distances_from_origin_to_amenities[origin].append([nearest_stop_map[origin][1],amenity_scores[amenity_type]])
+                except Exception:
+                    distances_from_origin_to_amenities[origin] = [[nearest_stop_map[origin][1],amenity_scores[amenity_type]]]
+                    
+    return distances_from_origin_to_amenities
+
+
+def get_bus_stop_amenity_rank(bus_stop_map):
+    bus_stops_with_amenity_rank = dict()
+
+    for amenity in AMENITIES:
+        amenity_results = get_amenity_gps(amenity)
+        for bus_stop in bus_stop_map:
+            distance = math.inf
+            
+            for amenity_gps in amenity_results:
+                straight_distance = geopy.distance.distance(amenity_gps, bus_stop_map[bus_stop])
+                if straight_distance <= 0.8:
+                    distance = min(straight_distance,distance) # calculate_distance(amenity_gps, bus_stop_map[bus_stop])
+            try:
+                bus_stops_with_amenity_rank[bus_stop_map[bus_stop]].append(distance)
+            except Exception:
+                bus_stops_with_amenity_rank[bus_stop_map[bus_stop]] = [distance]
+
+
+    return bus_stops_with_amenity_rank
+
+
+def scale_score(score):
+    
+    if score <= 0.100:
+        scaled_score = 1.6                        
+    elif score <= 0.200:
+        scaled_score = 1.4
+    elif score <= 0.300:
+        scaled_score = 1.2
+    elif score <= 0.400:
+        scaled_score = 1
+    elif score <= 0.500:
+        scaled_score = 0.8
+    elif score <= 0.600:
+        scaled_score = 0.6
+    elif score <= 0.700:
+        scaled_score = 0.4
+    elif score <= 0.800:
+        scaled_score = 0.2
+                    
+    return scaled_score
+
+
+def get_amenity_gps(amenity):
+    amenity_gps = []
     list_size = 0
     prev_list_size = -1
     page_num = 0
-    while list_size != prev_list_size:
-        if amenity_coordinates:
-            prev_list_size = list_size
-        page_num +=1
 
-        url = 'https://www.yellowpages.ca/search/si/%s/%s/%s+BC/rci-%s%%2C+BC'%(page_num,amenity,city,city)
-        response=requests.get(url)
+    requests.packages.urllib3.disable_warnings()
+    requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS += ':HIGH:!DH:!aNULL'
+    try:
+        requests.packages.urllib3.contrib.pyopenssl.util.ssl_.DEFAULT_CIPHERS += ':HIGH:!DH:!aNULL'
+    except AttributeError:
+        # no pyopenssl support used / needed / available
+        pass
 
-        amenity_coordinates.extend(re.findall(r'coordinates\D+(.*?)]',str(response.content)))
-        list_size = len(amenity_coordinates)
-    return amenity_coordinates
+##    while list_size != prev_list_size:
+##        if amenity_gps:
+##            prev_list_size = list_size
+    page_num +=1
+
+    url = 'https://www.yellowpages.ca/search/si/%s/%s/%s+BC/rci-%s%%2C+BC'%(page_num,amenity,city,city)
+    print(url)
+    response=requests.get(url,verify=False)
+
+    amenity_gps.extend(re.findall(r'coordinates\D+(.*?)]',str(response.content)))
+    list_size = len(amenity_gps)
+    return amenity_gps
 
 
-def calc_dist(cord1,cord2):
+def calculate_network_distance(cord1,cord2):
     router = Router("foot")
     start = router.findNode(cord1[0],cord1[1]) # Find start and end nodes
     end = router.findNode(cord2[0],cord2[1])
@@ -83,64 +186,58 @@ def calc_dist(cord1,cord2):
     return distance
 
 
-def rank_addresses(address_map, bus_stop_map):
-    t00 = time.time()
-
-    address_rankings = dict()
-    '''Sort by number of bus stops within 400m'''        
-    for address in address_map:
-        start_lat_lon = address_map[address]
-        stops_within_range = 0
-        for bus_stop in bus_stop_map:
-            end_lat_lon = bus_stop_map[bus_stop]
-            
-            stop_distance = geopy.distance.distance(address_map[address], bus_stop_map[bus_stop])
-            
-            if stop_distance < 0.6:
-                try:    distance = calc_dist(start_lat_lon,end_lat_lon)
-                except: pass
-
-                if distance <= 100:
-                    stops_within_range += 1.6                        
-                elif distance <= 200:
-                    stops_within_range += 1.4
-                elif distance <= 300:
-                    stops_within_range += 1.2
-                elif distance <= 400:
-                    stops_within_range += 1
-                elif distance <= 500:
-                    stops_within_range += 0.8
-                elif distance <= 600:
-                    stops_within_range += 0.6
-                elif distance <= 700:
-                    stops_within_range += 0.4
-                elif distance <= 800:
-                    stops_within_range += 0.2
-                
-        address_rankings[address] = stops_within_range
-        
-##        if counter > 200000:
-##            print(time.time() - t00)
-##            break
-        
-    to_be_ranked = dict(sorted(address_rankings.items(), key=lambda x:x[1]))
+def get_closest_stop(target_map, bus_stop_map):
+    counter = 0
     
-    return to_be_ranked
+    nearest_stop_map = dict()
+    
+    broke_count = 0
+    net_count= 0
+    bus_list = [x for x in bus_stop_map]
+    #print(bus_list)
+    print('target_map',len(target_map))
+    
+    '''Sort by number of bus stops within 400m'''
+    for target in target_map:
+        distance_to_closest_stop = math.inf
+        start_lat_lon = target_map[target]
+        
+        '''get nearest neighbor'''
+        tree = spatial.KDTree(bus_list)
+        
+        closest_stop = bus_list[int(tree.query([target_map[target]])[1])]
+        
+        distance_to_closest_stop = geopy.distance.distance(start_lat_lon, closest_stop)
+        
+        '''check if nearest neighbor is within distance'''
+        if distance_to_closest_stop < 0.6:
+            try:
+                distance = distance_to_closest_stop#calculate_network_distance(start_lat_lon, closest_stop)
+            except Exception:
+                broke_count+=1
+                print('network broke at : %s | %s' % (counter,broke_count))
+                continue
+            
+            nearest_stop_map[start_lat_lon] = [closest_stop, distance]
+
+    return nearest_stop_map
 
 
 def parse_source_data(source_folder):
     bus_stop_map = dict()
-    address_map = dict()
+    target_map = dict()
     route_map = dict()
+    grid_map = dict()
     
     with open(os.path.join(source_folder,bus_stop_file)) as bus_stop_points,\
          open(os.path.join(source_folder,address_file)) as address_points,\
-         open(os.path.join(source_folder,route_file)) as route_points:
-             
+         open(os.path.join(source_folder,route_file)) as route_points,\
+         open(os.path.join(source_folder,grid_file)) as grid_points:
+            
         address_lines = address_points.read()
         bus_stop_lines = bus_stop_points.readlines()
         route_lines = route_points.read()
-        
+        grid_lines = grid_points.readlines()
         '''Parse bus stop coords'''
         for bus_line in bus_stop_lines:
             if not bus_line.split(',')[3].isalpha():
@@ -148,24 +245,34 @@ def parse_source_data(source_folder):
                 bus_lon = float(bus_line.split(',')[4])
                 bus_stop_map[bus_line.split(',')[1]] = (bus_lat, bus_lon)
                 
-        '''Parse address street and gps coordinate'''
+        '''Parse target street and gps coordinate'''
         raw_address_info = re.findall(r'FULL_ADD(.*?) ] } }',address_lines)#[0:100]
 
+        '''parse address points'''
         for address_line in raw_address_info:
             street = address_line.split(':')[1].split(',')[0]
             raw_coords = address_line.split(':')[-1].replace('[','').strip().split(',')
 
             address_lat = float(raw_coords[1])
             address_lon = float(raw_coords[0])
-            address_map[street] = (address_lat,address_lon)
+            target_map[street] = (address_lat,address_lon)
 
+        '''Parse Route Points'''
         route_coords = re.findall(r'<coordinates>(.*?)</coordinates>',route_lines)
         route_names = re.findall(r'Route: \d+',route_lines)
         for route_coord, route_name in zip(route_coords,route_names):
             try:    route_map[route_name].append(route_coord.strip('<coordinates>').split(' '))
-            except: route_map[route_name] = [route_coord.strip('<coordinates>').split(' ')]
-                        
-    return address_map, bus_stop_map,route_map
+            except Exception: route_map[route_name] = [route_coord.strip('<coordinates>').split(' ')]
+
+        '''Parse Grid Points'''
+        for grid_point in grid_lines:
+            grid_latitude = grid_point.split(',')[1].strip()
+            grid_longitude = grid_point.split(',')[0].strip()
+            if not grid_latitude.isalpha():
+##                print(grid_latitude)
+                grid_map[(float(grid_latitude),float(grid_longitude))] = (float(grid_latitude),float(grid_longitude))
+        
+    return target_map, bus_stop_map,route_map, grid_map
 
     
 def rank_routes(route_info):
@@ -177,7 +284,7 @@ def rank_routes(route_info):
         for street in route_info[route]:
             average_route_frequency = get_route_frequency(route_info[route][street])
             if average_route_frequency == 0:
-                average_route_frequency=999
+                average_route_frequency = 999
             total_average += average_route_frequency
 
         route_ranking[route] = total_average / len(route_info[route])
@@ -242,10 +349,10 @@ def get_route_info(city_urls):
 
                 try:
                     route_map[route_number][street].append(time)
-                except:
+                except Exception:
                     try:
                         route_map[route_number][street] = [time]
-                    except:
+                    except Exception:
                         route_map[route_number] = {street: [time]}
     return route_map
 
@@ -331,13 +438,18 @@ if __name__ == '__main__':
     bus_stop_file = 'kel_busstops.csv'
     route_file = 'kel_routes.kml'
     address_file = 'Address_Points.geojson'
+    grid_file = 'city_of_kelowna_grid_coordinates.txt'
     
     '''Scraping setup'''
     url = 'https://www.bctransit.com/'
     test_city = 'kelowna'
 
-    city = 'Victoria'
-    amenities = ['restaurants','gyms','groceries','banks']
+    city = 'Kelowna'
+    AMENITIES = ['restaurants','gyms','groceries','banks']
+
+    range_dict = {(50.019847274822695, -119.37532151949802) : [[1200,2700,0],[5210,8440,25]],
+                  (49.91729642268884, -119.33119246289769)  : [[11500,10000,0]],
+                  (49.82303160146931, -119.41015669675217)  : [[6000,4000,50]],}
     
     main()
 
